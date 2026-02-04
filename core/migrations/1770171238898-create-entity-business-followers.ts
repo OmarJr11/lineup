80ccaf5bce79cd9397acabf7545f752f017a5b60
OmarJr11 <omaryoeljr@gmail.com>
2026-02-03 22:48:57 -0400

    :hammer: CU-86c80tv0x feat(Products) Set followers in businesses

--- core/migrations/1770171238898-create-entity-business-followers.ts
+++ core/migrations/1770171238898-create-entity-business-followers.ts
@@ -4,7 +4,7 @@ export class CreateEntityBusinessFollowers1770171238898 implements MigrationInte
     name = 'CreateEntityBusinessFollowers1770171238898'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "businesses" RENAME COLUMN "likes" TO "followers"`);
+        await queryRunner.query(`ALTER TABLE "businesses" ADD "followers" bigint NOT NULL DEFAULT 0`);
         await queryRunner.query(`CREATE TYPE "public"."business_followers_status_enum" AS ENUM('active', 'inactive', 'pending', 'suspended', 'deleted', 'completed', 'rejected', 'approved', 'failed', 'denied')`);
         await queryRunner.query(`CREATE TABLE "business_followers" ("creation_date" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "modification_date" TIMESTAMP WITH TIME ZONE DEFAULT now(), "creation_ip" character varying(50), "modification_ip" character varying(50), "creation_coordinate" point, "modification_coordinate" point, "id" BIGSERIAL NOT NULL, "id_business" bigint NOT NULL, "id_creation_user" bigint NOT NULL, "status" "public"."business_followers_status_enum" NOT NULL DEFAULT 'active', CONSTRAINT "PK_06d15e521178ba8e8eeca77aa07" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE UNIQUE INDEX "IDX_6a013031155a4082058e84c002" ON "business_followers" ("id_business", "id_creation_user") `);
@@ -18,7 +18,7 @@ export class CreateEntityBusinessFollowers1770171238898 implements MigrationInte
         await queryRunner.query(`DROP INDEX "public"."IDX_6a013031155a4082058e84c002"`);
         await queryRunner.query(`DROP TABLE "business_followers"`);
         await queryRunner.query(`DROP TYPE "public"."business_followers_status_enum"`);
-        await queryRunner.query(`ALTER TABLE "businesses" RENAME COLUMN "followers" TO "likes"`);
+        await queryRunner.query(`ALTER TABLE "businesses" DROP COLUMN "followers"`);
     }
 
 }
