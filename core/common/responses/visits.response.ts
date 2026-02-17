07ba9469d8c61c0a34ec467c0bc6b835cb65f31d
OmarJr11 <omaryoeljr@gmail.com>
2026-02-16 21:09:40 -0400

    :hammer: master feat(Master) Merge master/develop

--- core/common/responses/visits.response.ts
+++ core/common/responses/visits.response.ts
@@ -1,22 +1,22 @@
 export const visitsResponses = {
     create: {
         noPermission: {
-            code: 2000700,
+            code: 2100100,
             status: false,
             message: 'You do not have the necessary permissions to perform this action.',
         },
         invalidVisitType: {
-            code: 2000701,
+            code: 2100101,
             status: false,
             message: 'Invalid visit type.',
         },
         error: {
-            code: 2000799,
+            code: 2100199,
             status: false,
             message: 'The visit could not be created, an error has occurred.',
         },
         success: {
-            code: 2100700,
+            code: 2110100,
             status: true,
             message: 'The visit has been successfully created.',
         },
